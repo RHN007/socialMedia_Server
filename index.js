fe9870f661d7c0947c8b127031d6e02f847b0703
RHN007 <rakibul.nayon@outlook.com>
2022-12-27 16:07:19 +0600

    adding files

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const userRoute = require('./router/users')
 const authRoute = require ('./router/auth')
-
+const postRoute = require("./router/post");
+const cors = require('cors');
+app.use(cors())
 mongoose.set('strictQuery', true)
-
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGO_URL, () => {
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log(' Connected to MongoDB')
 });
 
@@ -19,11 +20,12 @@ mongoose.connect(process.env.MONGO_URL, () => {
 
 app.use(express.json())
 app.use(helmet())
-app.use(morgan())
+app.use(morgan("common"))
 
 
 app.use('/api/users', userRoute)
 app.use('/api/auth', authRoute)
+app.use("/api/posts", postRoute);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Homepage')
